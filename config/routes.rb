d60d83827620f880ef8e63e602332c78b2552dc6
meshack <maphosoms77@gmail.com>
2017-04-18 13:04:36 +0200

    hhg

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   resources :events
   devise_for :users
   root 'welcome#index'
-  default_url_options  :host => "localhost:3000"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
