f577a28acd749a8e5f1be7d4e5b78cbf383af518
meshack <maphosoms77@gmail.com>
2017-04-19 15:18:28 +0200

    contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :events
   devise_for :users
   root 'welcome#index'
+  get 'contact', to: 'welcome#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
